7cf0295c529d4bd2bd9f467399b33e07f9ade3f9
João Victor Amaral Diniz de Souza <jv-amaralds@hotmail.com>
2025-09-09 19:11:10 -0300

    Implementando a inserção de um produto através de um corpo de requisição

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -4,10 +4,7 @@ import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
@@ -24,8 +21,13 @@ public class ProductController {
         return service.findById(id);
     }
 
-    @GetMapping()
+    @GetMapping
     public Page<ProductDTO> findAll(Pageable pageable) {
         return service.findAll(pageable);
+    }
+
+    @PostMapping
+    public ProductDTO insert(@RequestBody ProductDTO dto) {
+        return service.insert(dto);
     }
 }

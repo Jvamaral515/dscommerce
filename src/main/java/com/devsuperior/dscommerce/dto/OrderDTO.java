d1b13af4b9e2ebdb516a8da522ff55ebf56ae68f
João Victor Amaral Diniz de Souza <jv-amaralds@hotmail.com>
2025-10-23 19:04:31 -0300

    Implementando o OrderDTO e a requisição Order by id

---
+++ src/main/java/com/devsuperior/dscommerce/dto/OrderDTO.java
@@ -0,0 +1,70 @@
+package com.devsuperior.dscommerce.dto;
+
+import com.devsuperior.dscommerce.entities.Order;
+import com.devsuperior.dscommerce.entities.OrderStatus;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+public class OrderDTO {
+
+    private Long id;
+    private Instant moment;
+    private OrderStatus status;
+    private ClientDTO client;
+    private PaymentDTO payment;
+    private Set<OrderItemDTO> items = new HashSet<>();
+
+    public OrderDTO() {
+    }
+
+    public OrderDTO(Long id, Instant moment, OrderStatus status, ClientDTO client, PaymentDTO payment) {
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+        this.client = client;
+        this.payment = payment;
+    }
+
+    public OrderDTO(Order entity) {
+        id = entity.getId();
+        moment = entity.getMoment();
+        status = entity.getStatus();
+        client = new ClientDTO(entity.getClient());
+        payment = (entity.getPayment() == null) ? null : new PaymentDTO(entity.getPayment());
+        entity.getItems().forEach(x -> items.add(new OrderItemDTO(x)));
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public ClientDTO getClient() {
+        return client;
+    }
+
+    public PaymentDTO getPayment() {
+        return payment;
+    }
+
+    public Set<OrderItemDTO> getItems() {
+        return items;
+    }
+
+    public Double getTotal() {
+        Double total = 0.0;
+        for (OrderItemDTO item : items) {
+            total += item.getSubTotal();
+        }
+        return total;
+    }
+}
